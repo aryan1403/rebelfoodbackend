74eac186dcc9a23ab9ba415b83084410c151917f
aryan1403 <aaryan14032006@email.com>
2022-08-04 16:24:06 +0530

    alloe access origin

--- index.mjs
+++ index.mjs
@@ -7,6 +7,12 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.post('/register', async (req, res) => {
     const body = JSON.parse(JSON.stringify(req.body));
     res.header("Access-Control-Allow-Origin", "*");
